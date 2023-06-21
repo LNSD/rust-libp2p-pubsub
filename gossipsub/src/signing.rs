915df631d848a01896950cafa851403a5885e156
Lorenzo Delgado <lnsdev@proton.me>
2023-06-22 00:04:30 +0200

    chore(gossipsub): remove gossipsub fork crate

--- gossipsub/src/signing.rs
+++
@@ -1,8 +0,0 @@
-pub use signer::{AuthorOnlySigner, Libp2pSigner, MessageSigner, NoopSigner, RandomAuthorSigner};
-pub use validator::{
-    AnonymousMessageValidator, MessageValidator, NoopMessageValidator, PermissiveMessageValidator,
-    StrictMessageValidator,
-};
-
-mod signer;
-mod validator;
--- gossipsub/tests/it_gossipsub.rs
+++
@@ -1,2 +0,0 @@
-mod gossipsub;
-mod testlib;
