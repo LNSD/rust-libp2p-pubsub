180d5ce7a8da5c6f29c0bb69f4025faeb24661e7
Lorenzo Delgado <lnsdev@proton.me>
2023-07-10 21:41:32 +0200

    feat(common): improve prost codec error handling

--- common/src/codec/prost_protobuf.rs
+++ common/src/codec/prost_protobuf.rs
@@ -4,23 +4,22 @@ use std::marker::PhantomData;
 
 use asynchronous_codec::{Decoder, Encoder};
 use bytes::BytesMut;
-use prost::{DecodeError, Message};
+use prost::Message;
 use unsigned_varint::codec::UviBytes;
 
 #[derive(Debug, thiserror::Error)]
-#[error("Failed to encode/decode message")]
-pub struct Error(#[from] std::io::Error);
+pub enum Error {
+    #[error("Maximum message length exceeded")]
+    MaxMessageLenExceeded,
 
-impl From<Error> for std::io::Error {
-    fn from(e: Error) -> Self {
-        e.0
-    }
-}
+    #[error("Length-prefix error: {0}")]
+    LengthPrefixError(std::io::Error),
 
-impl From<DecodeError> for Error {
-    fn from(e: DecodeError) -> Self {
-        Self(e.into())
-    }
+    #[error("Protobuf decode error")]
+    ProtobufDecodeError(#[from] prost::DecodeError),
+
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
 }
 
 /// [`Codec`] implements [`Encoder`] and [`Decoder`], uses [`unsigned_varint`]
@@ -42,7 +41,7 @@ impl<In, Out> Codec<In, Out> {
         uvi.set_max_len(max_message_len_bytes);
         Self {
             uvi,
-            phantom: PhantomData::default(),
+            phantom: PhantomData,
         }
     }
 }
@@ -52,10 +51,12 @@ impl<In: Message, Out> Encoder for Codec<In, Out> {
     type Error = Error;
 
     fn encode(&mut self, item: Self::Item, dst: &mut BytesMut) -> Result<(), Self::Error> {
-        let mut encoded_msg = BytesMut::with_capacity(item.encoded_len());
-        item.encode(&mut encoded_msg)
+        let mut encoded_frame = BytesMut::with_capacity(item.encoded_len());
+        item.encode(&mut encoded_frame)
             .expect("BytesMut to have sufficient capacity.");
-        self.uvi.encode(encoded_msg.freeze(), dst)?;
+        self.uvi
+            .encode(encoded_frame.freeze(), dst)
+            .map_err(|_| Error::MaxMessageLenExceeded)?;
 
         Ok(())
     }
@@ -66,10 +67,12 @@ impl<In, Out: Message + Default> Decoder for Codec<In, Out> {
     type Error = Error;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        Ok(self
-            .uvi
-            .decode(src)?
-            .map(|msg| Message::decode(msg))
-            .transpose()?)
+        let bytes = self.uvi.decode(src).map_err(|e| match e.kind() {
+            std::io::ErrorKind::PermissionDenied => Error::MaxMessageLenExceeded,
+            std::io::ErrorKind::Other => Error::LengthPrefixError(e),
+            _ => unreachable!("Unexpected error kind: {:?}", e.kind()),
+        })?;
+
+        Ok(bytes.map(|bytes| Message::decode(bytes)).transpose()?)
     }
 }
