4d83f138a4f7e8f8ad962e189b1c1673317129c6
Lorenzo Delgado <lnsdev@proton.me>
2023-09-01 04:02:59 +0200

    feat(pubsub): added behaviour event

--- pubsub/src/lib.rs
+++ pubsub/src/lib.rs
@@ -1,8 +1,10 @@
+pub use event::Event;
 pub use message_id::{default_message_id_fn, MessageId, MessageIdFn};
 pub use subscription::{Subscription, SubscriptionBuilder};
 pub use topic::{Hasher, IdentityHash, Sha256Hash, Topic, TopicHash};
 
 mod conn_handler;
+mod event;
 mod framing;
 mod message_id;
 mod services;
