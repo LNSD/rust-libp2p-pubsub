54cf8b9ea6c4ac1eeaf9f86a4d220a9bebcd7e61
Lorenzo Delgado <lnsdev@proton.me>
2023-09-01 04:55:51 +0200

    feat(pubsub): added behaviour scaffolding

--- pubsub/src/lib.rs
+++ pubsub/src/lib.rs
@@ -1,9 +1,11 @@
+pub use behaviour::Behaviour;
 pub use config::Config;
 pub use event::Event;
 pub use message_id::{default_message_id_fn, MessageId, MessageIdFn};
 pub use subscription::{Subscription, SubscriptionBuilder};
 pub use topic::{Hasher, IdentityHash, Sha256Hash, Topic, TopicHash};
 
+mod behaviour;
 mod config;
 mod conn_handler;
 mod event;
