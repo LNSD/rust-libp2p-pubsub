4363671f98e6f85f1dc14d34d5c2378f5d571eea
Lorenzo Delgado <lnsdev@proton.me>
2023-09-11 18:20:22 +0200

    refactor(pubsub-core): move all proto conversion logic to framing service

--- pubsub-core/src/framing/message.rs
+++ pubsub-core/src/framing/message.rs
@@ -7,7 +7,7 @@ use crate::topic::TopicHash;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct Message {
-    proto: MessageProto,
+    pub(crate) proto: MessageProto,
 }
 
 impl Message {
@@ -113,53 +113,5 @@ impl Message {
 
     pub fn set_key(&mut self, key: Option<impl Into<Vec<u8>>>) {
         self.proto.key = key.map(|bytes| bytes.into().into());
-    }
-}
-
-impl From<MessageProto> for Message {
-    /// Convert from a [`MessageProto`] into a [`Message`]. Additionally. sanitize the protobuf
-    /// message by removing optional fields when empty.
-    #[must_use]
-    fn from(mut proto: MessageProto) -> Self {
-        // A non-present data field should be interpreted as an empty payload.
-        if proto.data.is_none() {
-            proto.data = Some(Bytes::new());
-        }
-
-        // An empty from field should be interpreted as not present.
-        if let Some(from) = proto.from.as_ref() {
-            if from.is_empty() {
-                proto.from = None;
-            }
-        }
-
-        // An empty seqno field should be interpreted as not present.
-        if let Some(seq_no) = proto.seqno.as_ref() {
-            if seq_no.is_empty() {
-                proto.seqno = None;
-            }
-        }
-
-        // An empty signature field should be interpreted as not present.
-        if let Some(signature) = proto.signature.as_ref() {
-            if signature.is_empty() {
-                proto.signature = None;
-            }
-        }
-
-        // An empty key field should be interpreted as not present.
-        if let Some(key) = proto.key.as_ref() {
-            if key.is_empty() {
-                proto.key = None;
-            }
-        }
-
-        Self { proto }
-    }
-}
-
-impl From<Message> for MessageProto {
-    fn from(message: Message) -> Self {
-        message.into_proto()
     }
 }
--- pubsub-core/src/framing/subopts.rs
+++ pubsub-core/src/framing/subopts.rs
@@ -1,5 +1,3 @@
-use libp2p_pubsub_proto::pubsub::SubOptsProto;
-
 use crate::topic::TopicHash;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -18,35 +16,6 @@ impl SubscriptionAction {
         match self {
             SubscriptionAction::Subscribe(topic_id) => (topic_id, true),
             SubscriptionAction::Unsubscribe(topic_id) => (topic_id, false),
-        }
-    }
-}
-
-impl From<SubOptsProto> for SubscriptionAction {
-    /// Convert a [`SubOptsProto`] into a [`SubscriptionAction`].
-    fn from(proto: SubOptsProto) -> Self {
-        match proto {
-            SubOptsProto {
-                subscribe: Some(subscribe),
-                topic_id: Some(topic_id),
-            } if subscribe => SubscriptionAction::Subscribe(TopicHash::from_raw(topic_id)),
-            SubOptsProto {
-                subscribe: Some(subscribe),
-                topic_id: Some(topic_id),
-            } if !subscribe => SubscriptionAction::Unsubscribe(TopicHash::from_raw(topic_id)),
-            _ => unreachable!("invalid SubOptsProto: {proto:?}"),
-        }
-    }
-}
-
-impl From<SubscriptionAction> for SubOptsProto {
-    /// Convert a [`SubscriptionAction`] into a [`SubOptsProto`].
-    fn from(action: SubscriptionAction) -> Self {
-        let (topic_id, subscribe) = action.into_pair();
-
-        Self {
-            subscribe: Some(subscribe),
-            topic_id: Some(topic_id.into_string()),
         }
     }
 }
